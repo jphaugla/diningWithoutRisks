d8fc361c969c0a8c85ca5cc2be8033dee57d9e5d
Haugland <jphaugla@3c22fba09f76.ant.amazon.com>
2021-03-01 09:46:04 -0600

    get IP in setEnvironment

--- template/setEnvironment.sh
+++ template/setEnvironment.sh
@@ -3,3 +3,4 @@
 export PROJECT_NAME=jphmenu
 #  staging bucket name to initially hold code uploaded from this github
 export S3_BUCKET=${PROJECT_NAME}-s3-stage-bucket
+export LOCAL_IP=`curl http://checkip.amazonaws.com/`
